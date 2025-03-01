599b73fa8096fe48363ad98e2aa0805f836fde5e
amarkopoulos <amarkopoulos@mtg-hellas.gr>
2025-03-01 11:17:44 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="m-2 p-2 border">
+    <h1 class="text-3xl font-bold">Welcome to the Andram nuxt-template!</h1>
+    <h2 class="text-2xl font-bold">This is a starter template with the following configured:</h2>
+    <ul class="list-disc list-inside mt-3">
+      <li>Nuxt</li>
+      <li>Typescript</li>
+      <li>Tailwind</li>
+      <li>Pinia</li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
